37aea4637f0eeede872ecb049f6dc925aab3cb0b
王泽昌 <624946829@qq.com>
2023-05-31 11:51:29 +0800

    配置

---
+++ day83/vue-demo/src/component/mybutton.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div @click="count++">我爱你</div>
+        <div>爱了 {{ count }} 次</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 3,
+        };
+    },
+};
+</script>
+
+<style scoped>
+.box {
+    border: 1px solid #333;
+}
+</style>
--- day83/vue-demo/src/main.js
+++ day83/vue-demo/src/main.js
@@ -1,4 +1,7 @@
-import Vue from 'vue'
+// import Vue from 'vue/dist/vue';//完整版vue包
+import Vue from 'vue';//node_modules\vue\dist\vue.runtime.common.js
+// 阉割版;阉割了模板编译器
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
@@ -6,3 +9,13 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// new Vue({
+//   el: "#app",
+//   components: {
+//     App,
+//   },
+//   template:
+//     `<div/>`
+  
+// });
