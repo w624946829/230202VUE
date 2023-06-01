5456ca32314bbf8a101eaeaec6e42e804ecab889
王泽昌 <624946829@qq.com>
2023-06-01 21:44:59 +0800

    todolist练习

---
+++ day83/vue-two/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="todo-container">
+        <div class="todo-wrap">
+            <Header></Header>
+            <Main :todos="todos"></Main>
+            <Footer :todos="todos"></Footer>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Main from "@/components/Main";
+import Footer from "@/components/Footer";
+
+export default {
+    name: "App",
+    components: {
+        Header,
+        Main,
+        Footer,
+    },
+    data() {
+        return {
+            todos: [
+                { id: 10001, content: "抽烟", isSel: true },
+                { id: 10002, content: "喝酒", isSel: true },
+                { id: 10003, content: "烫头", isSel: true },
+            ],
+        };
+    },
+};
+</script>
+
+<style scoped>
+/*app*/
+.todo-container {
+    width: 600px;
+    margin: 0 auto;
+}
+.todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+}
+</style>
---
+++ day84/vue-two/src/components/Main/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <ul class="todo-main">
+          <Item
+            v-for="(item,index) in todos"
+            :key ='index'
+            :todo="item"
+          ></Item>
+          
+        </ul>
+  
+</template>
+
+<script>
+import Item from '@/components/Item';
+export default {
+    name:"Main",
+    components:{
+        Item
+    },
+    props:{
+         todos: {
+            type: Array,
+            required: true,
+        },
+    }
+}
+
+</script>
+
+<style scoped>
+/*main*/
+  .todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+  }
+  
+  .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+</style>
