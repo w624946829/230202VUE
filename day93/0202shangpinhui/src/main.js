f08e4fb3cfc9b41b40cb785c5a985183e2a73444
王泽昌 <624946829@qq.com>
2023-06-14 08:39:40 +0800

    ....

---
+++ day93/0202shangpinhui/src/main.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: 王泽昌 624946829@qq.com
+ * @Date: 2023-06-12 08:34:27
+ * @LastEditors: 王泽昌 624946829@qq.com
+ * @LastEditTime: 2023-06-12 11:58:59
+ * @FilePath: \0202shangpinhui\src\main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from '@/store'
+import TypeNav from '@/components/TypeNav'
+import '@/mock'
+
+Vue.config.productionTip = false
+Vue.component('TypeNav',TypeNav)
+new Vue({
+    el:'#app',
+    router,
+    store,
+    render:h =>h(App),
+})
