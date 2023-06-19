f4cebd7e8a615be9b3d3935dc5e7d1186f1071f1
王泽昌 <624946829@qq.com>
2023-06-19 09:14:34 +0800

    新的一天

---
+++ day96/0202shangpinhui/src/main.js
@@ -0,0 +1,31 @@
+/*
+ * @Author: 王泽昌 624946829@qq.com
+ * @Date: 2023-06-12 08:34:27
+ * @LastEditors: 王泽昌 624946829@qq.com
+ * @LastEditTime: 2023-06-17 11:36:16
+ * @FilePath: \0202shangpinhui\src\main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from '@/store'
+import TypeNav from '@/components/TypeNav'
+import Pagination from '@/components/Pagination'
+
+import '@/mock'
+
+Vue.config.productionTip = false
+Vue.component('TypeNav',TypeNav)
+Vue.component('Pagination',Pagination)
+
+new Vue({
+    el:'#app',
+    router,
+    store,
+    //事件总线处理搜索框残留
+    beforeCreate(){
+        Vue.prototype.$bus = this
+    },
+    render:h =>h(App),
+})
---
+++ day96/0202shangpinhui/src/pages/Register/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>我是register组件</h1>
+</template>
+
+<script>
+  export default {
+    name:'Register'
+  }
+</script>
+
+<style lang="less" scoped>
+   h1{
+    background-color: green;
+    height: 300px;
+  }
+</style>
