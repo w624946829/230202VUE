44b4dff715810a44298fb58358517f052db0ab96
王泽昌 <624946829@qq.com>
2023-06-26 00:20:12 +0800

    axios请求练习

--- day94/0202shangpinhui - test1/src/mock/index.js
+++
@@ -1,6 +0,0 @@
-import Mock from 'mockjs'
-import slide from './slide.json'
-import floor from './floor.json'
-Mock.mock('https://www.0202.com/slide','get',{code:200,ok:true,data:slide,message:'成功'})
-
-Mock.mock('https://www.0202.com/floor','get',{code:200,ok:true,data:floor,message:'成功'})
